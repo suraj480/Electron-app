7967f3eafb8d5ad0afed462a16f516e830f2dec3
suraj480 <surajmaurya991@gmail.com>
2023-10-10 07:29:35 +0530

    child file

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,8 +2,15 @@ Electron.js
 1.Electron is an open source software development
 
 Topics of Series
-1.set up
+1.set up:- 
+(i)create html file 
+(ii)create main.js file
+(iii)npm init
+(iv)npm i electron --dev --save
+(v) write start as "start": "electron ."
+(vi)npm run start
 2.Hello world
+
 3.Make Basic and small app
 4.IPC(Inter-process-communication)
 5.Menu
@@ -13,3 +20,8 @@ Topics of Series
 9.Curd App
 10.Electron with react.
  
+
+ Tips:-
+ ctrl+shift+i
+ main process run in background
+ render process run in UI level
--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-const {app,BrowserWindow}=require('electron')
-function createWindow()
-{
-    const win=new BrowserWindow({
-        width:800,
-        height:600,
-        webPreferences:{
-            nodeIntegration:true
-        }
-    })
-    win.loadFile("index.html")
+const { app, BrowserWindow } = require("electron");
+console.log("i am main process");
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: "#FFEBCD",
+    // frame:false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  // const child =new BrowserWindow()
+  // child.loadFile('index.html')
+  // child.show()
+  win.loadFile("index.html");
+  win.webContents.openDevTools();
 }
 app.whenReady().then(createWindow);
