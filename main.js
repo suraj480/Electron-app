59cd927760633be1e8ac46e1ee9bc59284d67f8a
suraj480 <surajmaurya991@gmail.com>
2023-10-11 23:48:22 +0530

    web contents and dialog box

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow ,dialog,globalShortcut} = require("electron");
 const windowStateKeeper=require('electron-window-state')
 let win;
 console.log("i am main process");
 function createWindow() {
   let mainWindowState=windowStateKeeper({
-    defaultHeight:800,
-    defaultWidth:500
+    defaultHeight:50,
+    defaultWidth:100
   })
   const win = new BrowserWindow({
     x:mainWindowState.x,
@@ -22,8 +22,32 @@ function createWindow() {
   // child.loadFile('index.html')
   // child.show()
   win.loadFile("index.html");
- // win.webContents.openDevTools();
+  win.webContents.openDevTools();
  mainWindowState.manage(win)
+ globalShortcut.register("shift+k",()=>{
+  dialog.showOpenDialog({
+    defaultPath:app.getPath('desktop')
+  })
+ })
+ //Open a dialog
+//  win.webContents.on("did-finish-load",()=>{
+//   dialog.showOpenDialog({
+//     defaultPath:app.getPath('desktop')
+//   })
+//  })
+let wc =win.webContents
+wc.on('dom-ready',()=>{
+  console.log("dom is ready")
+})
+wc.on('did-finish-load',()=>{
+  console.log("did-finish-load")
+})
+wc.on('new-window',()=>{
+  console.log("new window open")
+})
+wc.on("before-input-event",()=>{
+  console.log("some key is pressed")
+})
 }
 // app.on('will-quit',(e)=>{
 //   console.log("call quit app")
