77ffafa05da67e068f39ba626abe06d57f92ac68
suraj480 <surajmaurya991@gmail.com>
2023-10-11 23:06:50 +0530

    window state

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const { app, BrowserWindow } = require("electron");
+const windowStateKeeper=require('electron-window-state')
+let win;
 console.log("i am main process");
 function createWindow() {
+  let mainWindowState=windowStateKeeper({
+    defaultHeight:800,
+    defaultWidth:500
+  })
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    x:mainWindowState.x,
+    y:mainWindowState.y,
+    defaultHeight:mainWindowState.height,
+    defaultWidth:mainWindowState.width,
     backgroundColor: "#FFEBCD",
     // frame:false,
     webPreferences: {
@@ -14,6 +22,24 @@ function createWindow() {
   // child.loadFile('index.html')
   // child.show()
   win.loadFile("index.html");
-  win.webContents.openDevTools();
+ // win.webContents.openDevTools();
+ mainWindowState.manage(win)
 }
-app.whenReady().then(createWindow);
+// app.on('will-quit',(e)=>{
+//   console.log("call quit app")
+//   e.preventDefault();
+// })
+//app.whenReady().then(createWindow);
+app.on('before-quit',()=>{
+  console.log("you are clossing the app")
+})
+app.on('browser-window-focus',()=>{
+  console.log("you are on app")
+})
+app.on('browser-window-blur',()=>{
+  console.log("you are unfocus app")
+})
+app.on('ready',()=>{
+  createWindow();
+  console.warn("app is ready you can write some code here")
+})
